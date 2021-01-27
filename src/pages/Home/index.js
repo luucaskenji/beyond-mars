14225b569849cae718cb298267d4af6cb4a2bbb8
Lucas Murakami <lkenjim98@gmail.com>
2021-01-27 19:05:13 -0300

    Getting data from nasa api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+import { Container } from './HomeStyles';
 
 export default function Home() {
+    const history = useHistory();
+
+    useEffect(() => {
+        const apiKey = process.env.REACT_APP_API_KEY;
+        let mostRecentDate;
+        
+        axios
+            .get(`https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity?api_key=${apiKey}`)
+            .then(r => {
+                mostRecentDate = r.data.photo_manifest.max_date;
+                _getPhotos();
+            })
+            .catch(() => {
+                alert('Erro ao carregar conteúdo');
+                history.push('/');
+            }); 
+        
+        const _getPhotos = () => {
+            axios
+            .get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${mostRecentDate}&api_key=${apiKey}`)
+            .then(r => console.log(r.data))
+            .catch(() => {
+                alert('Erro ao carregar conteúdo');
+                history.push('/');
+            });
+        }
+    }, []);
+
     return (
-        <h1>Home Page</h1>
+        <Container>Home Page</Container>
     );
 }
